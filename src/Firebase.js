e0e4b4900cf19d3c230ab1db4f6cab68082133ee
Vaibhav Gupta <vaibhav16feb1997@gmail.com>
2020-10-10 15:48:12 +0530

    10oct15:48

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBycUr3lGhMlk_XIOhOlBBE9vNWGRIMZ1k",
+    authDomain: "instagram-clone-41688.firebaseapp.com",
+    databaseURL: "https://instagram-clone-41688.firebaseio.com",
+    projectId: "instagram-clone-41688",
+    storageBucket: "instagram-clone-41688.appspot.com",
+    messagingSenderId: "685428692344",
+    appId: "1:685428692344:web:a4ba7a97228c779d54248d",
+    measurementId: "G-HY2NJYWW6C"
+  };
