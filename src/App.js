687593bb321219ed6461495240dfc9e33e85da89
Vaibhav Gupta <vaibhav16feb1997@gmail.com>
2020-10-10 19:23:32 +0530

    10oct19:23

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Post from "./Post"
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+     {/*Header*/ }
+    <div className="app__header">
+<img className="app__headerImage" 
+src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
+</div>
+{/*Story*/ }
+      {/*Post*/ }
+<Post />
+      {/*post */ }
+      {/*post*/ }
+      {/**/ }
     </div>
   );
 }
