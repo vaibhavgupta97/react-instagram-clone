d3a2786cd6ba918945bd0571640392bc8f451d1d
Vaibhav Gupta <vaibhav16feb1997@gmail.com>
2020-10-11 22:00:58 +0530

    11oct22:00

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,19 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyBycUr3lGhMlk_XIOhOlBBE9vNWGRIMZ1k",
-    authDomain: "instagram-clone-41688.firebaseapp.com",
-    databaseURL: "https://instagram-clone-41688.firebaseio.com",
-    projectId: "instagram-clone-41688",
-    storageBucket: "instagram-clone-41688.appspot.com",
-    messagingSenderId: "685428692344",
-    appId: "1:685428692344:web:a4ba7a97228c779d54248d",
-    measurementId: "G-HY2NJYWW6C"
-  };
+  apiKey: "AIzaSyCSnVSAVboicGRH2Oak3heDQxi0crEUbGA",
+  authDomain: "instagram-clone-a5a2b.firebaseapp.com",
+  databaseURL: "https://instagram-clone-a5a2b.firebaseio.com",
+  projectId: "instagram-clone-a5a2b",
+  storageBucket: "instagram-clone-a5a2b.appspot.com",
+  messagingSenderId: "482629061605",
+  appId: "1:482629061605:web:03ec6097faa90bdf67fd0c",
+  measurementId: "G-8YD8CK433Y"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage=firebase.storage();
+
+export { db, auth,storage };
